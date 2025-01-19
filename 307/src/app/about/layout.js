452643e7a19386352cefb5b392c0d3e04fa3b706
Abbas Khosravi <khosravi1977@gmail.com>
2025-01-19 13:01:02 +0330

    307

--- 307/src/app/about/layout.js
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children, modal }) {
-    return (
-        <div className="container mx-auto">
-            {children}
-            {modal}
-        </div>
-    );
-}
